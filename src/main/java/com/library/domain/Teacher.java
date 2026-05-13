1ae08ce3132317b3fcfed4a3ee60f271d5074c15
SEVEER <SEVEER221@outlook.com>
2026-05-12 21:38:20 -0400

    fields

--- src/main/java/com/library/domain/Teacher.java
+++ src/main/java/com/library/domain/Teacher.java
@@ -1,6 +1,11 @@
 package com.library.domain;
 
 public class Teacher extends User{
+    private String teacherId;
+    private static final int MAX_BOOKS = 10;
+
+
+
     public Teacher(String userName, List<Item> borrowedItems) {
         super(userName, borrowedItems);
     }
