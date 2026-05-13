261c564df0eff1ef452e4fe8d345e98ac5cd8cad
SEVEER <SEVEER221@outlook.com>
2026-05-12 21:34:33 -0400

    redid constructor

--- src/main/java/com/library/domain/Student.java
+++ src/main/java/com/library/domain/Student.java
@@ -4,8 +4,9 @@ public class Student extends User{
     private String studentId;
     private static final int MAX_BOOKS = 5;
 
-    public Student(String userName, List<Item> borrowedItems) {
+    public Student(String userName, List<Item> borrowedItems, String studentId) {
         super(userName, borrowedItems);
+        this.studentId = studentId;
     }
 
     @Override
