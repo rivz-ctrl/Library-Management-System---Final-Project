889febbaf50bcfbb07fbd92d21659520583a2b2f
SEVEER <SEVEER221@outlook.com>
2026-05-12 23:22:20 -0400

    constructor

--- src/main/java/com/library/domain/Magazine.java
+++ src/main/java/com/library/domain/Magazine.java
@@ -4,4 +4,9 @@ public class Magazine extends Item{
     private String publisher;
     private int issueNumber;
 
+    public Magazine(String title, ItemStatus status, String publisher, int issueNumber) {
+        super(title, status);
+        this.publisher = publisher;
+        this.issueNumber = issueNumber;
+    }
 }
