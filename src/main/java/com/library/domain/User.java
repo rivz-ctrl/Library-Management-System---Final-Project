747c2e154c995aef40ff3e06fc20b91ba7733a59
SEVEER <SEVEER221@outlook.com>
2026-05-12 21:24:53 -0400

    cons

--- src/main/java/com/library/domain/User.java
+++ src/main/java/com/library/domain/User.java
@@ -5,4 +5,12 @@ public abstract class User {
     protected String userId;
     protected String userName;
     protected List<Item> borrowedItems;
+
+    int nextId = 1;
+
+    public User(String userId, String userName, List<Item> borrowedItems) {
+        this.userId = String.format("%04d", nextId++);
+        this.userName = userName;
+        this.borrowedItems = borrowedItems;
+    }
 }
