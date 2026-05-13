396a58363ba5144ded1c411f178d3b03da56902d
SEVEER <SEVEER221@outlook.com>
2026-05-12 21:48:39 -0400

    constructor

--- src/main/java/com/library/domain/Item.java
+++ src/main/java/com/library/domain/Item.java
@@ -5,4 +5,12 @@ public abstract class Item {
     protected String itemId;
     protected String title;
     protected ItemStatus status;
+
+    int nextId = 1;
+
+    public Item(String title, ItemStatus status) {
+        this.itemId = String.format("%04d", nextId++);
+        this.title = title;
+        this.status = status;
+    }
 }
