dedf9f8d7798b4e12f5514fbb6f06d8e024fa14a
SEVEER <SEVEER221@outlook.com>
2026-05-12 23:09:00 -0400

    constructor

--- src/main/java/com/library/domain/Book.java
+++ src/main/java/com/library/domain/Book.java
@@ -6,5 +6,15 @@ public class Book extends Item{
     private String genre;
     private static final String ISBN_CODE = " ";
 
+    public Book(String title, ItemStatus status, String isbn, String author, String genre) {
+        super(title, status);
+        this.isbn = isbn;
+        this.author = author;
+        this.genre = genre;
+    }
 
+    @Override
+    public String getItemInfo() {
+        return "";
+    }
 }
