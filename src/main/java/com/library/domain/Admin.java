4931d59a9e65888efc7d1e73ba77a29f46007375
SEVEER <SEVEER221@outlook.com>
2026-05-12 21:41:45 -0400

    nevermind ig T-T

--- src/main/java/com/library/domain/Admin.java
+++ src/main/java/com/library/domain/Admin.java
@@ -1,29 +1,12 @@
 package com.library.domain;
 
 public class Admin extends User{
-    private String teacherId;
-    private static final int MAX_ITEMS = 10;
-
-
-    public Teacher(String userName, List<Item> borrowedItems, String teacherId) {
+    public Admin(String userName, List<Item> borrowedItems) {
         super(userName, borrowedItems);
-        this.teacherId = teacherId;
     }
 
     @Override
     public int getBorrowCap() {
-        return MAX_ITEMS;
+        return 0;
     }
-
-    public String getTeacherId() {
-        return teacherId;
-    }
-
-    @Override
-    public String toString() {
-        return "Teacher{" +
-                "teacherId='" + teacherId + '\'' +
-                ", userId='" + userId + '\'' +
-                ", userName='" + userName + '\'' +
-                '}';
 }
