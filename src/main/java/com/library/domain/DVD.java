318d3cac0d9f3f43d90c78c2481904dbc42c41db
SEVEER <SEVEER221@outlook.com>
2026-05-12 23:20:02 -0400

    constructor

--- src/main/java/com/library/domain/DVD.java
+++ src/main/java/com/library/domain/DVD.java
@@ -4,4 +4,9 @@ public class DVD extends Item{
     private String director;
     private int duration;
 
+    public DVD(String title, ItemStatus status, String director, int duration) {
+        super(title, status);
+        this.director = director;
+        this.duration = duration;
+    }
 }
