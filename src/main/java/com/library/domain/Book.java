7c93564965f095803cf611ac420cf1fea20c1b01
SEVEER <SEVEER221@outlook.com>
2026-05-12 23:08:18 -0400

    fields

--- src/main/java/com/library/domain/Book.java
+++ src/main/java/com/library/domain/Book.java
@@ -1,4 +1,10 @@
 package com.library.domain;
 
 public class Book extends Item{
+    private String isbn;
+    private String author;
+    private String genre;
+    private static final String ISBN_CODE = " ";
+
+
 }
