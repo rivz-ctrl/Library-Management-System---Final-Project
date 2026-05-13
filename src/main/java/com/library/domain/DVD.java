30009525ad8f17fa1a8e7069e4289a15fa6e5f8b
SEVEER <SEVEER221@outlook.com>
2026-05-12 23:20:21 -0400

    getters

--- src/main/java/com/library/domain/DVD.java
+++ src/main/java/com/library/domain/DVD.java
@@ -9,4 +9,12 @@ public class DVD extends Item{
         this.director = director;
         this.duration = duration;
     }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
 }
