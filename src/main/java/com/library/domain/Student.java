d1321886528c9da5fa885324de1b48f6e470a782
SEVEER <SEVEER221@outlook.com>
2026-05-12 21:30:45 -0400

    user class

--- src/main/java/com/library/domain/Student.java
+++ src/main/java/com/library/domain/Student.java
@@ -1,4 +1,13 @@
 package com.library.domain;
 
-public class Student {
+public class Student extends User{
+
+    public Student(String userName, List<Item> borrowedItems) {
+        super(userName, borrowedItems);
+    }
+
+    @Override
+    public int getBorrowCap() {
+        return 0;
+    }
 }
