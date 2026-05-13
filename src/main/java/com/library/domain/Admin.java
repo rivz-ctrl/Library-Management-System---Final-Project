9ae0da1bf55e38eb3a33293d355b4bad81cc120f
SEVEER <SEVEER221@outlook.com>
2026-05-12 21:31:17 -0400

    admin class

--- src/main/java/com/library/domain/Admin.java
+++ src/main/java/com/library/domain/Admin.java
@@ -1,4 +1,12 @@
 package com.library.domain;
 
-public class Admin {
+public class Admin extends User{
+    public Admin(String userName, List<Item> borrowedItems) {
+        super(userName, borrowedItems);
+    }
+
+    @Override
+    public int getBorrowCap() {
+        return 0;
+    }
 }
