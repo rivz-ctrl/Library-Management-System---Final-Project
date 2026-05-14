7d785b44778e2f5dc0c37757526907558aa073ab
SEVEER <SEVEER221@outlook.com>
2026-05-14 15:16:47 -0400

    byTitle desc/asc

--- src/main/java/com/library/util/sorting/ItemSort.java
+++ src/main/java/com/library/util/sorting/ItemSort.java
@@ -1,4 +1,27 @@
 package com.library.util.sorting;
 
+import com.library.domain.Item;
+
+import java.util.Comparator;
+
 public class ItemSort {
+
+    /**
+     * sorts items ascendingly alphabetically by title
+     * @return sorting
+     */
+    public static Comparator<Item> byTitleAscend() {
+        return Comparator.comparing(item -> item.getTitle().toLowerCase());
+    }
+
+    /**
+     * sorts items descendingly alphabetically by title
+     * @return sorting
+     */
+    public static Comparator<Item> byTitleDescend() {
+        return Comparator.comparing((Item item) -> item.getTitle().toLowerCase()).reversed();
+    }
+
+
+
 }
