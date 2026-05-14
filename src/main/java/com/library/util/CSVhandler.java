ef144f401d5834a59c812bda9750245aecf676f7
SEVEER <SEVEER221@outlook.com>
2026-05-13 22:19:01 -0400

    fields

--- src/main/java/com/library/util/CSVhandler.java
+++ src/main/java/com/library/util/CSVhandler.java
@@ -1,5 +1,16 @@
 package com.library.util;
 
 public class CSVhandler {
-    private static int ITEM_TYPE = 0;
+    private static final int ITEM_DURATION = 0;
+    private static final int ITEM_TYPE = 0;
+    private static final int ITEM_ID = 0;
+    private static final int ITEM_DIRECTOR = 0;
+    private static final int ITEM_STATUS = 0;
+    private static final int ITEM_TITLE = 0;
+    private static final int ITEM_ISBN = 0;
+    private static final int ITEM_PUBLISHER = 0;
+    private static final int ITEM_AUTHOR = 0;
+    private static final int ITEM_GENRE = 0;
+    private static final int ITEM_ISSUE_NUM = 0;
+
 }
