3052ff86499bb5dfdb9a865833b5f112546026a4
SEVEER <SEVEER221@outlook.com>
2026-05-14 20:12:18 -0400

    isbn valid, invalid, null

--- src/test/java/com/library/DifferentTestClasses/BookTest.java
+++ src/test/java/com/library/DifferentTestClasses/BookTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class BookTest {
     @BeforeEach
@@ -23,5 +23,26 @@ public class BookTest {
         assertEquals("Music", book.getGenre());
         assertEquals("9781254307141", book.getIsbn());
         assertEquals(Item.ItemStatus.AVAILABLE, book.getStatus());
+    }
+
+    @Test
+    @DisplayName("9781254307141 is a valid ISBN")
+    public void testCheckISBNValid() {
+        Book book = new Book("Rest In Bass", Item.ItemStatus.AVAILABLE, "9781254307141", "Che", "Music");
+        assertTrue(book.checkISBN());
+    }
+
+    @Test
+    @DisplayName("1234567890123 is an invalid ISBN, cuz doesnt start with 97 8 or 9)")
+    public void testCheckISBNInvalid() {
+        Book book = new Book("Not real book", Item.ItemStatus.AVAILABLE, "1234567890123", "Author", "Genre");
+        assertFalse(book.checkISBN());
+    }
+
+    @Test
+    @DisplayName("null -> invalid ISBN")
+    public void testCheckISBNnull() {
+        Book book = new Book("null ISBN", Item.ItemStatus.AVAILABLE, null, "Author", "Genre");
+        assertFalse(book.checkISBN());
     }
 }
