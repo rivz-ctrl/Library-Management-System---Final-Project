a6ac4a7cda346d392cf7b1fd471dfca96a0fb693
SEVEER <SEVEER221@outlook.com>
2026-05-14 20:20:25 -0400

    getting Iteminfo, setting status

--- src/test/java/com/library/DifferentTestClasses/DVDTest.java
+++ src/test/java/com/library/DifferentTestClasses/DVDTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class DVDTest {
 
@@ -23,5 +24,21 @@ public class DVDTest {
         assertEquals("Ryan Coogler", dvd.getDirector());
         assertEquals(240, dvd.getDuration());
         assertEquals(Item.ItemStatus.AVAILABLE, dvd.getStatus());
+    }
+
+    @Test
+    @DisplayName("getItemInfo contains title and director")
+    public void testGetItemInfo() {
+        DVD dvd = new DVD("Waves", Item.ItemStatus.AVAILABLE, "Ryan Coogler", 240);
+        assertTrue(dvd.getItemInfo().contains("Waves"));
+        assertTrue(dvd.getItemInfo().contains("Ryan Coogler"));
+    }
+
+    @Test
+    @DisplayName("status from available to borrowed")
+    public void testSettingStatus() {
+        DVD dvd = new DVD("Waves", Item.ItemStatus.AVAILABLE, "Ryan Coogler", 240);
+        dvd.setStatus(Item.ItemStatus.BORROWED);
+        assertEquals(Item.ItemStatus.BORROWED, dvd.getStatus());
     }
 }
